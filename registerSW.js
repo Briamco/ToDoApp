696196f0aa37a1a54305f1038b37b644cce11b1f
Briamco <63083603+Briamco@users.noreply.github.com>
2024-10-28 12:07:59 -0400

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://briamco.github.io/ToDoApp/sw.js', { scope: 'https://briamco.github.io/ToDoApp/' })})}
